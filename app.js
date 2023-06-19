f945f6017e3c89dd4aa7efd27089088268213e17
pavel <gonpav@gmail.com>
2023-06-19 13:31:05 +0800

    Changed proj structure

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("Connected to MongoDB Atlas");
-});
+}); 
+
 
 
 
